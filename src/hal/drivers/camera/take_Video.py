00904d41b659bd980d1bf8cc1a8acb5d13aa1643
Lucas Cabral <lcc4@cin.ufpe.br>
2017-11-15 19:57:17 -0300

    drivers: Changing the storage directory for camera output.

--- src/hal/drivers/camera/take_Video.py
+++ src/hal/drivers/camera/take_Video.py
@@ -5,11 +5,15 @@ from time import sleep
 camera = PiCamera()
 camera.resolution = (620,480)
 camera.framerate = 10
-camera.start_recording('1.h264')
+camera.start_preview()
+
+camera.start_recording('/home/pi/Desktop/ESS/src/comm/to_stream/video1.h264')
 camera.wait_recording(1)
 
-for i in range (2,86400)
-    camera.split_recording('%d.h264' % i)
+for i in range (2,86400):
+    camera.split_recording('/home/pi/Desktop/ESS/src/comm/to_stream/video%d.h264' % i)
     camera.wait_recording(1)
 
 camera.stop_recording()
+
+camera.stop_preview()
