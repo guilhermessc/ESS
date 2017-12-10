9b086de230b3b00ed91472f078b5b8625e0bc66e
Guilherme Sales Santa Cruz <gssc@cin.ufpe.br>
2017-12-10 15:36:50 -0300

    bugfix: run Stream deamon properly

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@
 touch stream_log
 touch take_video_log
 
-./src/comm/stream.sh >> stream_log &
+cd src/comm/
+./stream.sh >> stream_log &
+cd ../../
+
 ./src/hal/drivers/camera/take_Video.py >> take_video_log &
 
