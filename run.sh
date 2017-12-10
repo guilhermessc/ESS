08defe400bb2e45202de93ceda48853fe3ff6cf9
Guilherme Sales Santa Cruz <gssc@cin.ufpe.br>
2017-12-10 15:13:18 -0300

    run: create log

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 
 # DEMO:
 
-./src/comm/stream.sh &
-./src/hal/drivers/camera/take_Video.py &
+touch stream_log
+touch take_video_log
+
+./src/comm/stream.sh >> stream_log &
+./src/hal/drivers/camera/take_Video.py >> take_video_log &
 
