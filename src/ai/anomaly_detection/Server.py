d668a38f4905c6d4d7921bef163ff97734fd1d61
Carlos Valadares <cfcv@cin.ufpe.br>
2017-12-10 19:21:06 -0300

    ai: Add server comunication with gps.

---
+++ src/ai/anomaly_detection/Server.py
@@ -0,0 +1,21 @@
+import socket
+
+class Server(object):
+    def __init__(self, port):
+        print('Server is on, port:',port)
+        self.__addr = ('192.168.15.9',port)
+        self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__sock.bind(self.__addr)
+        self.__sock.listen(10)
+
+    def wait_connection(self):
+        print('Waiting connection...')
+        self.__con, self.__client = self.__sock.accept()
+    
+    def receive(self, b):
+        while True:
+            data = self.__con.recv(b)
+            if len(data) > 0:
+                return data
+    
+
