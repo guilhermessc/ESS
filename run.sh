9153026d9a4941c232b46e378f1b4899ca1febdc
Guilherme Sales Santa Cruz <guilhermessantacruz@gmail.com>
2017-12-10 22:58:44 +0000

    run: Update with gps deamon

--- run.sh
+++ run.sh
@@ -4,11 +4,18 @@
 
 touch stream_log
 touch take_video_log
+touch gps_driver_log
 
 cd src/comm/
 ./stream.sh > ../../stream_log &
 cd ../../
 
-cd src/hal/drivers/camera
+cd src/hal/drivers/camera/
 ./take_Video.py > take_video_log &
 cd ../../../../
+
+
+# the gps driver is a simulation on the keyboard
+cd src/hal/drivers/
+./gps.py > gps_driver_log
+
