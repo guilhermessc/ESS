58f5a38de9d757bcec425ecf0e928f22996357cf
Guilherme Sales Santa Cruz <gssc@cin.ufpe.br>
2017-12-10 16:41:10 -0300

    bugfix: fix path at run for take_video

--- src/hal/drivers/camera/take_Video.py
+++ src/hal/drivers/camera/take_Video.py
@@ -4,14 +4,14 @@ from time import sleep
 
 camera = PiCamera()
 camera.resolution = (620,480)
-camera.framerate = 10
+camera.framerate = 30
 camera.start_preview()
-
-camera.start_recording('../../../comm/to_stream/video1.h264')
+i = 1
+camera.start_recording('/../../comm/to_stream/%010d.h264' % i)
 camera.wait_recording(1)
 
 for i in range (2,86400):
-    camera.split_recording('../../../comm/to_stream/video%d.h264' % i)
+    camera.split_recording('/../../comm/to_stream/%010d.h264' % i)
     camera.wait_recording(1)
 
 camera.stop_recording()
